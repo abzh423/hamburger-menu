9d1b9a45246dbef48d09307ccee888feefd00f0e
Abulkhair <abzh423@protonmail.com>
2022-01-13 13:31:18 +0600

    Popover Menu Example

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,47 @@
-import React from "react";
+import { useLocation } from "react-router-dom";
 import PopoverExample from "./PopoverExample";
 
 function App() {
+  type Navigation = {
+    id: number;
+    name: string;
+    href: string;
+    activeTextBlack: string;
+  };
+
+  const { pathname } = useLocation();
+  const currentPage = pathname.split("/")[1];
+
+  const navigation: Navigation[] = [
+    {
+      id: 1,
+      name: "Home",
+      href: "/",
+      activeTextBlack: currentPage === "" ? "text-black" : "",
+    },
+    {
+      id: 2,
+      name: "Portfolio",
+      href: "/portfolio",
+      activeTextBlack: currentPage === "portfolio" ? "text-black" : "",
+    },
+    {
+      id: 3,
+      name: "Blog",
+      href: "/blog",
+      activeTextBlack: currentPage === "blog" ? "text-black" : "",
+    },
+    {
+      id: 4,
+      name: "Contacts",
+      href: "contacts",
+      activeTextBlack: currentPage === "contacts" ? "text-black" : "",
+    },
+  ];
+
   return (
     <div>
-      <PopoverExample />
+      <PopoverExample navigation={navigation} />
     </div>
   );
 }
--- src/PopoverExample.tsx
+++ src/PopoverExample.tsx
@@ -1,22 +1,81 @@
-import { Popover } from "@headlessui/react";
-import { MenuIcon } from "@heroicons/react/solid";
+import { Fragment, FC } from "react";
+import { Popover, Transition } from "@headlessui/react";
+import { MenuIcon, XIcon } from "@heroicons/react/solid";
+import { Link } from "react-router-dom";
 import "./PopoverExample.css";
 
-function PopoverExample() {
+type Navigation = {
+  id: number;
+  name: string;
+  href: string;
+  activeTextBlack: string;
+};
+
+type Props = {
+  navigation: Navigation[];
+};
+
+type Open = {
+  open: boolean;
+};
+
+const PopoverExample: FC<Props> = ({ navigation }) => {
+  const buttonHandler = ({ open }: Open) => (
+    <>
+      {open ? <XIcon className="w-6 h-6" /> : <MenuIcon className="w-6 h-6" />}
+    </>
+  );
+
+  const mapNavigation = navigation.map((item) => {
+    return (
+      <li key={item.id} className="first:mt-0 mt-2 sm:mt-0">
+        <Link to={item.href} className={item.activeTextBlack}>
+          {item.name}
+        </Link>
+      </li>
+    );
+  });
+
   return (
     <Popover>
-      {({ open }) => (
-        <>
-          <Popover.Button>
-            <MenuIcon className="h-6 w-6" />
-          </Popover.Button>
-          <Popover.Panel>
-            <h1>Hello World</h1>
-          </Popover.Panel>
-        </>
-      )}
+      <div className="hamburger sm:hidden w-7 h-7 flex flex-col justify-center items-center cursor-pointer">
+        <Popover>
+          <Transition
+            show={true}
+            enter="duration-150 ease-out"
+            enterFrom="opacity-0 scale-95"
+            enterTo="opacity-100 scale-100"
+            leave="duration-100 ease-in"
+            leaveFrom="opacity-100 scale-100"
+            leaveTo="opacity-0 scale-95"
+          >
+            <Popover.Button className="p-1 mt-1 rounded-md bg-gray-200">
+              {buttonHandler}
+            </Popover.Button>
+          </Transition>
+          <Transition
+            as={Fragment}
+            enter="duration-150 ease-out"
+            enterFrom="opacity-0 scale-95"
+            enterTo="opacity-100 scale-100"
+            leave="duration-100 ease-in"
+            leaveFrom="opacity-100 scale-100"
+            leaveTo="opacity-0 scale-95"
+          >
+            <Popover.Panel className="absolute left-1/2 -translate-x-1/2 z-10 w-11/12 mt-3 ring-1 ring-black ring-opacity-5 rounded-lg">
+              <div className="rounded-lg shadow-md bg-white">
+                <div>
+                  <ul className="px-4 pt-4 pb-5 text-gray-500">
+                    {mapNavigation}
+                  </ul>
+                </div>
+              </div>
+            </Popover.Panel>
+          </Transition>
+        </Popover>
+      </div>
     </Popover>
   );
-}
+};
 
 export default PopoverExample;
